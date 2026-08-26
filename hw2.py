fb06fa420e511e5f9dc016d7029ce4dd1d57c037
Anthony Martinez <am130200@student.wpsok.org>
2026-08-26 12:47:58 -0500

    hm2

---
+++ hw2.py
@@ -0,0 +1,31 @@
+#Anthony Martinez
+#5th hour
+#HW2
+
+integerVar1 = 4
+stringVar2 = "Box"
+booleanVar3 = True
+stringVar4 = "Nineteen"
+stringVar5 = "Rope"
+integerVar6 = 19
+stringVar7 = "19"
+integerVar8 = 2
+booleanVar9 = True
+booleanVar10 = False
+stringvar11 ="True"
+stringVar12 = "False"
+integerVar13 = 5
+stringVar14 = "The"
+stringVar15 = "Game"
+booleanVar16 = False
+integerVar17 = 3
+integerVar18 = 2
+integerVar19 = 1
+stringVar20 = stringvar11
+stringVar21 = stringVar12
+stringVar22 = stringVar4
+stringVar23 = stringVar14
+stringVar24 = stringVar15
+integerVar25 = 6
+
+print(integerVar1+integerVar6+integerVar8+integerVar13+integerVar17+integerVar18+integerVar19+integerVar25)
